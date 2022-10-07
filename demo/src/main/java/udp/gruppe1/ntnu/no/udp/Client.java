5293d1d0e4fb53e71a99de611f04c988f10c668f
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-10-07 13:31:04 +0200

    Added message parameter

--- demo/src/main/java/udp/gruppe1/ntnu/no/udp/Client.java
+++ demo/src/main/java/udp/gruppe1/ntnu/no/udp/Client.java
@@ -7,24 +7,27 @@ import java.rmi.UnexpectedException;
 
 public class Client {
 
-    private Client(){}
-
-    public static DatagramPacket sendAndReceive(String hostname, int port) throws UnexpectedException {
-    try{
-        InetAddress address = InetAddress.getByName(hostname);
-        DatagramSocket socket = new DatagramSocket();
-        byte[] buffer = new byte[512];
- 
-        DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, port);
-        socket.send(request);
-    
-        DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-        socket.receive(response);
-        
-        return response;
-    }catch(Exception e){
-        throw new UnexpectedException("Some of your data may be wrong");
+    private Client() {
     }
+
+    public static String sendAndReceive(String hostname, int port, String message)
+            throws UnexpectedException, IllegalArgumentException {
+        if (port > 0 || hostname == null || hostname.isEmpty() || message.isEmpty()) {
+            throw new IllegalArgumentException("Some of the argument is not valid");
+        }
+        byte[] buffer = new byte[1024];
+
+        try {
+            InetAddress address = InetAddress.getByName(hostname);
+            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+            DatagramPacket request = new DatagramPacket(message.getBytes(), buffer.length, address, port);
+            try (DatagramSocket socket = new DatagramSocket()) {
+                socket.send(request);
+                socket.receive(response);
+            }
+            return new String(response.getData(), 0, response.getLength());
+        } catch (Exception e) {
+            throw new UnexpectedException("Some of your data may be wrong");
+        }
     }
 }
- 

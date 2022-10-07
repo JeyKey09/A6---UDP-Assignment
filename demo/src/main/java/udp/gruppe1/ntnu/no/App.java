e32470b46c184869c6efb8a16a76147c064cd109
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-10-07 13:56:23 +0200

    Did my part

--- demo/src/main/java/udp/gruppe1/ntnu/no/App.java
+++ demo/src/main/java/udp/gruppe1/ntnu/no/App.java
@@ -1,5 +1,7 @@
 package udp.gruppe1.ntnu.no;
 
+import udp.gruppe1.ntnu.no.udp.Client;
+
 /**
  * Hello world!
  *
@@ -8,10 +10,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String hostadress = "129.241.152.12";
+        int port = 1234;
+        try {
+            String task = Client.sendAndReceive(hostadress, port, "task");
+            String responseMessage = isQuestion(task)+" "+wordCount(task);
+            if(Client.sendAndReceive(hostadress, port, responseMessage).equals("ok")){
+                System.out.println("We did it!");
+            } else{
+                System.out.println("We did not do it!");
+            }
+        } catch (Exception e) {
+            System.out.println("We fucked up!");
+        }
     }
 
-    public int wordCount(String text){
+    public static int wordCount(String text){
         if(text.isBlank()){
             return 0;
         }
@@ -21,12 +35,12 @@ public class App
         return text.split(" ").length;
     }
 
-    public String isQuestion(String text){
+    public static String isQuestion(String text){
         if(text.contains("?")){
-            return "Question";
+            return "question";
         }
         if(text.contains("!")){
-            return "Statement";
+            return "statement";
         }
         return "neither";
     }

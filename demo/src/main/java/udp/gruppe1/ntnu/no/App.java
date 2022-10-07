e17ca001521e862f21d2b8ebf92cb7c4dd6599d6
Johannes <johakv@stud.ntnu.no>
2022-10-07 13:39:32 +0200

    Made initial version of app class

--- demo/src/main/java/udp/gruppe1/ntnu/no/App.java
+++ demo/src/main/java/udp/gruppe1/ntnu/no/App.java
@@ -1,13 +1,37 @@
 package udp.gruppe1.ntnu.no;
 
+import java.lang.reflect.Array;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+    public int wordCount(String text){
+        if(text.isBlank()){
+            return 0;
+        }
+        if(!text.contains(" ")){
+            return 1;
+        }
+        return text.split(" ").length;
+    }
+
+    public String isQuestion(String text){
+        if(text.contains("?")){
+            return "Question";
+        }
+        if(text.contains("!")){
+            return "Statement";
+        }
+        return "neither";
+    }
+
 }

003b23cb7e13f3f4a4140f5d90d9b67f71e06f87
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-10-07 13:19:54 +0200

    Added som try and catch

--- demo/src/main/java/udp/gruppe1/ntnu/no/udp/Client.java
+++ demo/src/main/java/udp/gruppe1/ntnu/no/udp/Client.java
@@ -1,24 +1,30 @@
 package udp.gruppe1.ntnu.no.udp;
 
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.rmi.UnexpectedException;
 
 public class Client {
 
     private Client(){}
 
-    public static DatagramPacket sendAndReceive(String hostname, int port) {
+    public static DatagramPacket sendAndReceive(String hostname, int port) throws UnexpectedException {
+    try{
+        InetAddress address = InetAddress.getByName(hostname);
+        DatagramSocket socket = new DatagramSocket();
+        byte[] buffer = new byte[512];
+ 
+        DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, port);
+        socket.send(request);
     
-    InetAddress address = InetAddress.getByName(hostname);
-    DatagramSocket socket = new DatagramSocket();
- 
-    byte[] buffer = new byte[512];
- 
-    DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, port);
-    socket.send(request);
-
-    DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-    return socket.receive(response);
+        DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+        socket.receive(response);
+        
+        return response;
+    }catch(Exception e){
+        throw new UnexpectedException("Some of your data may be wrong");
+    }
     }
 }
  

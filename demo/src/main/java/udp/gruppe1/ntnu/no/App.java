ce168a507b08ba026ebafeddd6236e126d393e7e
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-10-10 09:18:30 +0200

    Replaced the multiple returns and added documentation

--- demo/src/main/java/udp/gruppe1/ntnu/no/App.java
+++ demo/src/main/java/udp/gruppe1/ntnu/no/App.java
@@ -5,51 +5,58 @@ import java.rmi.UnexpectedException;
 import udp.gruppe1.ntnu.no.udp.Client;
 
 /**
- * Hello world!
+ * Gets and completes the task, and answers the server 3 times with the answer
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws UnexpectedException
-    {
-        try(Client client = new Client()) {
+public class App {
+    public static void main(String[] args) throws Exception {
+        try (Client client = new Client()) {
             String hostadress = "129.241.152.12";
             int port = 1234;
-            for(int i = 0; i < 3; i++){
-                try {
-                    String task = client.sendAndReceive(hostadress, port, "task");
-                    String responseMessage = isQuestion(task)+" "+wordCount(task);
-                    if(client.sendAndReceive(hostadress, port, responseMessage).equals("ok")){
-                        System.out.println("We did it!");
-                    }
-                    else{
-                        throw new UnexpectedException("We fucked up on the task"+task+"\n Sent response as: "+responseMessage);
-                    }
-                } catch (Exception e) {
-                    System.out.println("We fucked up!");
+            for (int i = 0; i < 3; i++) {
+                String task = client.sendAndReceive(hostadress, port, "task");
+                String responseMessage = isQuestion(task) + " " + wordCount(task);
+                if (client.sendAndReceive(hostadress, port, responseMessage).equals("ok")) {
+                    System.out.println("We did it!");
+                } else {
+                    throw new UnexpectedException(
+                            "We fucked up on the task" + task + "\n Sent response as: " + responseMessage);
                 }
             }
         } catch (Exception e) {
-            throw new UnexpectedException("Something went wrong");
-        } 
+            throw e;
+        }
     }
 
-    public static int wordCount(String text){
-        if(text.split(" ").length > 1){
-            return text.split(" ").length;
+    /**
+     * Finding the amount of word contained in a sentence
+     * 
+     * @param text The sentence to find the word count
+     * @return A integer with the amount of words found in the sentence
+     */
+    public static int wordCount(String text) {
+        int count = 0;
+        if (text.split(" ").length > 1) {
+            count = text.split(" ").length;
+        } else if (text.matches(".*[a-zA-Z].*")) {
+            count = 1;
         }
-        if(text.matches(".*[a-zA-Z].*")){
-            return 1;
-        }
-        return 0;
-        
+        return count;
+
     }
 
-    public static String isQuestion(String text){
-        if(text.contains("?")){
-            return "question";
+    /**
+     * Finds out if a sentence is a question or a statement
+     * 
+     * @param text the text to check
+     * @return String with the value "statement" or "question"
+     */
+    public static String isQuestion(String text) {
+        String answer = "statement";
+        if (text.contains("?")) {
+            answer = "question";
         }
-        return "statement";
+        return answer;
     }
 
 }
--- demo/src/main/java/udp/gruppe1/ntnu/no/udp/Client.java
+++ demo/src/main/java/udp/gruppe1/ntnu/no/udp/Client.java
@@ -3,23 +3,41 @@ package udp.gruppe1.ntnu.no.udp;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.rmi.UnexpectedException;
 
+/**
+ * Client is used for Assignment A6 as a UDP sender.
+ * It creates a socket for opening
+ */
 public class Client implements java.lang.AutoCloseable {
 
     private DatagramSocket socket;
 
-    public Client() throws UnexpectedException {
-        try {
-            this.socket = new DatagramSocket();
-        } catch (Exception e) {
-            throw new UnexpectedException("Couldn't create socket");
-        }
+    /**
+     * Creates a new instance of a client
+     * 
+     * @throws SocketException if it can't create a socket
+     */
+    public Client() throws SocketException {
+        this.socket = new DatagramSocket();
     }
 
+    /**
+     * Sends and receives a package between the {@hostname} and the client and
+     * returns the message
+     * 
+     * @param hostname The ip-adresse or hostadresse
+     * @param port     The port to send it at
+     * @param message  A optional message to be sent to the receiver
+     * @return The message sent from the server
+     * @throws UnexpectedException      if it can't send the package
+     * @throws IllegalArgumentException if the port is less then 0, the hostname is
+     *                                  null or hostname is empty
+     */
     public String sendAndReceive(String hostname, int port, String message)
             throws UnexpectedException, IllegalArgumentException {
-        if (port < 0 || hostname == null || hostname.isEmpty() || message.isEmpty()) {
+        if (port < 0 || hostname == null || hostname.isEmpty()) {
             throw new IllegalArgumentException("Some of the argument is not valid");
         }
         DatagramPacket response;
@@ -33,6 +51,17 @@ public class Client implements java.lang.AutoCloseable {
         return new String(response.getData(), 0, response.getLength());
     }
 
+    /**
+     * A private function for the actual sending and receiving to make
+     * sendAndReceive have less complexity.
+     * 
+     * It has a static buffer on 1024 Bytes
+     * 
+     * @param request A DatagramPacket to be sent
+     * @return Returns a DatagramPacket from the server
+     * @throws UnexpectedException if it can't send/receive the package or if the
+     *                             socket has other problem
+     */
     private DatagramPacket useSocket(DatagramPacket request) throws UnexpectedException {
         byte[] buffer = new byte[1024];
         DatagramPacket response;
